a48c864b9b025873215c39cb6f9b48f2f9b00a97
Herdis Maria <herdismaria@herdiss-macbook-pro.home>
2017-05-07 19:36:18 +0200

    Styled About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,22 @@
 var About =  (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the About page!</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        This a weather application built on React.
+        I built this while learning React
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - this was the JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openWeathermap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   )
 };
