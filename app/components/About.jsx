a437efff2b0c28fdea3f892997379ade21440f13
Herdis Maria <herdismaria@herdiss-macbook-pro.home>
2017-05-10 21:02:49 +0200

    Css added

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@
 var About =  (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>
         This a weather application built on React.
         I built this while learning React
